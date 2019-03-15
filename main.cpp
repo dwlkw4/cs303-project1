9947a9931a93debbfe66a7bafc56f9389b02f699
bradstarnes <starnesjackb@outlook.com>
2019-03-14 23:17:21 -0500

    Added Algorithm.md file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ *  Project Name: Project #1
+ *
+ *  Names: Brad Starnes, David Leatherwood, Adam Smith
+ *
+ *  Course: CS - 303 => Data Structures
+ *
+ *  Instructor: Professor Kuhail
+ *
+ *  Objective: Using queues, design a system that allows the circulation of books to employees.
+ *
+ *  Algorithm: See file named, "Algorithm.md"
+ *
+ *
+ *
+ *
+ */
