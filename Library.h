98383b0226a8263cd18f44e3b5f0e2e508344342
bradstarnes <starnesjackb@outlook.com>
2019-03-27 06:39:32 -0500

    Added private class members to Library header file

--- Library.h
+++ Library.h
@@ -5,6 +5,12 @@
 #ifndef CS303_PROJECT1_LIBRARY_H
 #define CS303_PROJECT1_LIBRARY_H
 
+#include <stdio.h>
+#include <string>
+#include <queue>
+#include <list>
+#include "book.cpp"
+
 
 class Library {
 
@@ -14,8 +20,12 @@ private:
     list<Employee*> universityEmployeeList;
 
 public:
+    Library();
+    ~Library();
 
-
+    void add_book(string newBook);
+    void add_employee(string newPerson);
+    void circulate_book(string bookToMove, Date dayOfMove);
 };
 
 
